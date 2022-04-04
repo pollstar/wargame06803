1c932519e2d7b0ddd65ba25398e176ce51a40387
Pavel Starovoitov <pollstar27@gmail.com>
2022-04-04 17:14:00 +0300

    resole warriors

--- test/ua/pollstar/softserve/BattleTest.java
+++ test/ua/pollstar/softserve/BattleTest.java
@@ -1,51 +1,21 @@
 package ua.pollstar.softserve;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BattleTest {
+    private Warrior warrior1, warrior2;
+    private Knight knight;
 
-    @Test
-    @DisplayName("test fight betwen Warrior and Warrior")
-    void fight001 () {
-        /*
-        chuck = Warrior()
-        bruce = Warrior()
-        fight(chuck, bruce) == True
-        chuck.is_alive == True
-        bruce.is_alive == False
-         */
-        var chuck = new Warrior();
-        var bruce = new Warrior();
-
-        var result = Battle.fight(chuck, bruce);
-
-        assertTrue(result);
-        assertTrue(chuck.isAlive);
-        assertFalse(bruce.isAlive);
-    }
-
-    @Test
-    @DisplayName("test fight betwen Knight and Warrior")
-    void fight002 () {
-        /*
-        carl = Knight()
-        dave = Warrior()
-
-        fight(dave, carl) == False
-        carl.is_alive == True
-        dave.is_alive == False
-        */
-        var carl = new Knight();
-        var dave = new Warrior();
-
-        var result = Battle.fight(dave, carl);
-
-        assertFalse(result);
-        assertTrue(carl.isAlive);
-        assertFalse(dave.isAlive);
+    @BeforeEach
+    void initEach(){
+        warrior1 = new Warrior();
+        warrior2 = new Warrior();
+        knight = new Knight();
     }
 
     @Test
@@ -53,17 +23,12 @@ class BattleTest {
     void fight1 () {
         /*
          *     "1. Fight": [
-         *         prepare_test(middle_code='''carl = Warrior()
+         * carl = Warrior()
          * jim = Knight()''',
          *                      test="fight(carl, jim)",
          *                      answer=False)
-         *                 ],
         */
-        var carl = new Warrior();
-        var jim = new Knight();
-
-        var result = Battle.fight(carl, jim);
-
+        var result = Battle.fight(warrior1, knight);
         assertFalse(result);
     }
 
@@ -72,16 +37,12 @@ class BattleTest {
     void fight2 () {
         /*
          *     "2. Fight": [
-         *         prepare_test(middle_code='''ramon = Knight()
+         * ramon = Knight()
          * slevin = Warrior()''',
          *                      test="fight(ramon, slevin)",
          *                      answer=True)
-         *                 ],
          */
-        var ramon = new Knight();
-        var slevin = new Warrior();
-
-        var result = Battle.fight(ramon, slevin);
+        var result = Battle.fight(knight, warrior1);
 
         assertTrue(result);
     }
@@ -91,19 +52,14 @@ class BattleTest {
     void fight3 () {
         /*
          *     "3. Fight": [
-         *         prepare_test(middle_code='''bob = Warrior()
+         * bob = Warrior()
          * mars = Warrior()
          * fight(bob, mars)''',
          *                      test="bob.is_alive",
          *                      answer=True)
-         *                 ],
          */
-        var bob = new Warrior();
-        var mars = new Warrior();
-
-        var result = Battle.fight(bob, mars);
-
-        assertTrue(bob.isAlive);
+        Battle.fight(warrior1, warrior2);
+        assertTrue(warrior1.getAlive());
     }
 
     @Test
@@ -111,19 +67,15 @@ class BattleTest {
     void fight4 () {
         /*
          *     "4. Fight": [
-         *         prepare_test(middle_code='''zeus = Knight()
+         * zeus = Knight()
          * godkiller = Warrior()
          * fight(zeus, godkiller)''',
          *                      test="zeus.is_alive",
          *                      answer=True)
-         *                 ],
          */
-        var zeus = new Knight();
-        var godkiller = new Warrior();
 
-        var result = Battle.fight(zeus, godkiller);
-
-        assertTrue(zeus.isAlive);
+        Battle.fight(knight, warrior1);
+        assertTrue(knight.getAlive());
     }
 
     @Test
@@ -131,19 +83,14 @@ class BattleTest {
     void fight5 () {
         /*
          *     "5. Fight": [
-         *         prepare_test(middle_code='''husband = Warrior()
+         * husband = Warrior()
          * wife = Warrior()
          * fight(husband, wife)''',
          *                      test="wife.is_alive",
          *                      answer=False)
-         *                 ],
          */
-        var husband = new Warrior();
-        var wife = new Warrior();
-
-        var result = Battle.fight(husband, wife);
-
-        assertFalse(wife.isAlive);
+        Battle.fight(warrior1, warrior2);
+        assertFalse(warrior2.getAlive());
     }
 
     @Test
@@ -151,19 +98,14 @@ class BattleTest {
     void fight6 () {
         /*
          *     "6. Fight": [
-         *         prepare_test(middle_code='''dragon = Warrior()
+         * dragon = Warrior()
          * knight = Knight()
          * fight(dragon, knight)''',
          *                      test="knight.is_alive",
          *                      answer=True)
-         *                 ],
          */
-        var dragon = new Warrior();
-        var knight = new Knight();
-
-        var result = Battle.fight(dragon, knight);
-
-        assertTrue(knight.isAlive);
+        Battle.fight(warrior1, knight);
+        assertTrue(knight.getAlive());
     }
 
     @Test
@@ -171,21 +113,15 @@ class BattleTest {
     void fight7 () {
         /*
          *     "7. Fight": [
-         *         prepare_test(middle_code='''unit_1 = Warrior()
+         * unit_1 = Warrior()
          * unit_2 = Knight()
          * unit_3 = Warrior()
          * fight(unit_1, unit_2)''',
          *                      test="fight(unit_2, unit_3)",
          *                      answer=False)
-         *                 ]
          */
-        var unit_1 = new Warrior();
-        var unit_2 = new Knight();
-        var unit_3 = new Warrior();
-
-        Battle.fight(unit_1, unit_2);
-        var result = Battle.fight(unit_2, unit_3);
-
+        Battle.fight(warrior1, knight);
+        var result = Battle.fight(knight, warrior2);
         assertFalse(result);
     }
 }
